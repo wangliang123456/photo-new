fa5cc7e2cbe1c370c490c71e274332e59aef00a1
亮 <liang.wang@wormpex.com>
2018-07-08 10:38:24 +0800

    add

--- src/main/java/com/wangliang/photo/PhotoApplication.java
+++ src/main/java/com/wangliang/photo/PhotoApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wangliang
+ */
 @SpringBootApplication
 @MapperScan("com.wangliang.photo.dao")
 public class PhotoApplication {
