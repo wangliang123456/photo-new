260aab2d4ef756288a36b6c2aaf5331f136b6cbe
亮 <liang.wang@wormpex.com>
2018-07-08 17:39:00 +0800

    add

--- src/main/java/com/wangliang/photo/service/impl/PhotoSpecServiceImpl.java
+++ src/main/java/com/wangliang/photo/service/impl/PhotoSpecServiceImpl.java
@@ -1,18 +1,21 @@
 package com.wangliang.photo.service.impl;
 
+import com.google.common.collect.Lists;
 import com.wangliang.photo.dao.PhotoSpecDao;
 import com.wangliang.photo.model.po.PhotoSpecPO;
 import com.wangliang.photo.model.vo.PhotoSpecVO;
 import com.wangliang.photo.service.PhotoSpecService;
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * @author wangliang
@@ -48,6 +51,16 @@ public class PhotoSpecServiceImpl implements PhotoSpecService {
      */
     @Override
     public List<PhotoSpecVO> queryByType(int type) {
-        return photoSpecDao;
+        List<PhotoSpecPO> photoSpecPOList = Lists.newArrayList();
+        List<PhotoSpecVO> photoSpecVOList = Lists.newArrayList();
+        photoSpecPOList = photoSpecDao.queryAllRecord(type);
+        if (CollectionUtils.isNotEmpty(photoSpecPOList)) {
+            photoSpecVOList = photoSpecPOList.stream().map(photoSpecPO -> {
+                PhotoSpecVO photoSpecVO = new PhotoSpecVO();
+                BeanUtils.copyProperties(photoSpecPO, photoSpecVO);
+                return photoSpecVO;
+            }).collect(Collectors.toList());
+        }
+        return photoSpecVOList;
     }
 }
