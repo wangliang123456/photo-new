52923405d47737e19b58f4e85df18377a175d884
亮 <liang.wang@wormpex.com>
2018-07-10 08:58:25 +0800

    add

--- src/main/java/com/wangliang/photo/dao/OrderDao.java
+++ src/main/java/com/wangliang/photo/dao/OrderDao.java
@@ -1,5 +1,7 @@
 package com.wangliang.photo.dao;
 
+import com.wangliang.photo.model.po.OrderPO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -7,5 +9,30 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface OrderDao {
+
+
+    /**
+     * 创建订单
+     *
+     * @param orderPO
+     * @return
+     */
+    OrderPO createOrder(@Param("obj") OrderPO orderPO);
+
+    /**
+     * 删除订单
+     *
+     * @param orderId
+     * @return
+     */
+    int deleteOrder(@Param("orderId") int orderId);
+
+    /**
+     * 查询订单
+     *
+     * @param orderId
+     * @return
+     */
+    OrderPO queryOrderById(@Param("orderId") int orderId);
 
 }
