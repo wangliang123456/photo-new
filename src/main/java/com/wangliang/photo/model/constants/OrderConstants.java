5d51d32f30bee65e96208520dae7b8ff83e66ab5
亮 <liang.wang@wormpex.com>
2018-07-11 23:04:06 +0800

    add

---
+++ src/main/java/com/wangliang/photo/model/constants/OrderConstants.java
@@ -0,0 +1,10 @@
+package com.wangliang.photo.model.constants;
+
+public interface OrderConstants {
+
+    interface OrderStatus {
+        int WAITING_PAY = 0;
+        int PAID = 1;
+        int DELETE = 2;
+    }
+}
